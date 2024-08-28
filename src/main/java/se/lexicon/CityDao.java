450360dae65e8d1675fc60d1fd5b110b95b0c484
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-08-28 11:15:44 +0200

    Created and implemented classes

--- src/main/java/se/lexicon/CityDao.java
+++ src/main/java/se/lexicon/CityDao.java
@@ -5,12 +5,16 @@ import java.util.List;
 public interface CityDao {
 
     City findById(int id);
-    List<City>findByCode(String code);
-    List<City>findByName(String name);
-    List<City>findAll();
+
+    List<City> findByCode(String code);
+
+    List<City> findByName(String name);
+
+    List<City> findAll();
+
     City add(City city);
+
     int delete(City city);
 
-
-    }
+}
 
